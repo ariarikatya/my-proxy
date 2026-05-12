ba45a460a0a20afa9a50b14a5bc6a8ee952b3b57
ariarikatya <ariarikaty@gmail.com>
2026-05-12 17:13:03 +0200

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -1,4 +1,14 @@
 export default async function handler(req, res) {
+    // --- ВСТАВЛЯЕМ CORS СЮДА ---
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+
+    // Ответ на предварительный запрос браузера
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
     if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
 
     // Твои данные из ЮKassa
