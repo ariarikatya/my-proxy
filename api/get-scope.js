525a1b5c07541489505b28b7b98ab82ea644fa5b
ariarikatya <ariarikaty@gmail.com>
2026-06-25 02:19:41 +0200

    Update get-scope.js

--- api/get-scope.js
+++ api/get-scope.js
@@ -22,27 +22,23 @@ export default async function handler(req, res) {
   const url = `https://amojo.amocrm.ru/v2/origin/custom/${channelId}/connect`;
   const date = new Date().toUTCString();
   
-  const body = JSON.stringify({ 
-  account_id: accountId,
-  secret: channelSecret 
-});
-  const contentMd5 = crypto.createHash('md5').update(body).digest('hex');
-  
-  // Собираем строку для подписи строго по документации API чатов
-  const signatureString = `POST\n${contentMd5}\napplication/json\n${date}\n/v2/origin/custom/${channelId}/connect`;
-  const signature = crypto.createHmac('sha1', channelSecret).update(signatureString).digest('hex');
+  const body = JSON.stringify({ account_id: accountId });
+const contentMd5 = crypto.createHash('md5').update(body).digest('hex');
 
-  try {
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Date': date,
-        'Content-MD5': contentMd5,
-        'X-Signature': signature
-      },
-      body: body
-    });
+// Подпись и заголовки остаются прежними, но убедись, что AMO_CHANNEL_SECRET в Vercel актуальный
+const signatureString = `POST\n${contentMd5}\napplication/json\n${date}\n/v2/origin/custom/${channelId}/connect`;
+const signature = crypto.createHmac('sha1', channelSecret).update(signatureString).digest('hex');
+
+const response = await fetch(url, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Date': date,
+    'Content-MD5': contentMd5,
+    'X-Signature': signature
+  },
+  body: body
+});
 
     const data = await response.json();
     return res.status(200).json({ 
